b43fa73a7d7ceb9c4a0c3f6aa19c621efade3ba2
小江编不出 <126128149+xiaojiangcantprogram@users.noreply.github.com>
2024-04-14 13:38:49 +0800

    Create QueueFactory.java

---
+++ 06-dailytasks/src/main/java/oy/tol/tra/06-dailytasks/src/main/java/oy/tol/tra/QueueFactory.java
@@ -0,0 +1,11 @@
+package oy.tol.tra;
+
+public class QueueFactory {
+
+    private QueueFactory() {
+    }
+
+    public static QueueInterface<Integer> createIntegerQueue(int capacity) {
+        return new QueueImplementation<>(capacity);
+    }
+}
