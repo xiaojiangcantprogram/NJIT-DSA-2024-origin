3a22dd2988cbf17aa5537b3018f00f67267e854e
小江编不出 <126128149+xiaojiangcantprogram@users.noreply.github.com>
2024-04-15 22:12:20 +0800

    Update StackFactory.java

--- 05-parenthesis-checker/src/main/java/oy/tol/tra/StackFactory.java
+++ 05-parenthesis-checker/src/main/java/oy/tol/tra/StackFactory.java
@@ -3,51 +3,48 @@ package oy.tol.tra;
 /**
  * This class instantiates different types of stacks implementing the {@code StackInterface} interface.
  * <p>
- * TODO: Students, implement the createCharacterStack method for instantiating {@code StackImplementation<Character>}
- * objects in the this task.
- * 
- * @author Antti Juustila
- * @version 1.0
+ * The class provides static methods to create instances of stack objects.
+ * </p>
  */
 public class StackFactory {
 
-   private StackFactory() {
-   }
-
-   /**
-    * Creates an instance of StackImplementation for Integer type.
-    * @return The stack object.
-    */
+    private StackFactory() {
+    }
+    /**
+     * Creates an instance of StackImplementation for Integer type.
+     *
+     * @return The stack object.
+     */
     public static StackInterface<Integer> createIntegerStack() {
-      return new StackImplementation<>();
-   }
+        return new StackImplementation<>();
+    }
 
-   /**
-    * Creates an instance of StackImplementation for Integer type.
-    * @param capacity Number of elements the stack can hold.
-    * @return The stack object.
-    */
-   public static StackInterface<Integer> createIntegerStack(int capacity) {
-      return new StackImplementation<>(capacity);
-   }
+    /**
+     * Creates an instance of StackImplementation for Integer type.
+     *
+     * @param capacity Number of elements the stack can hold.
+     * @return The stack object.
+     */
+    public static StackInterface<Integer> createIntegerStack(int capacity) {
+        return new StackImplementation<>(capacity);
+    }
 
-   /**
-    * Instantiates a stack of Characters using the stack default constructor.
-    * TODO: Students, implement this method in this task.
-    * @return The stack implementation holding Characters.
-    */
-   public static StackInterface<Character> createCharacterStack() {
+    /**
+     * Creates an instance of StackImplementation for Character type.
+     *
+     * @return The stack object.
+     */
+    public static StackInterface<Character> createCharacterStack() {
+        return new StackImplementation<>();
+    }
 
-      return stack;
-   }
-
-   /**
-    * Instantiates a stack of Characters using the stack default constructor.
-    * TODO: Students, implement this method in this task.
-    * @return The stack implementation holding Characters.
-    */
+    /**
+     * Creates an instance of StackImplementation for Character type.
+     *
+     * @param capacity Number of elements the stack can hold.
+     * @return The stack object.
+     */
     public static StackInterface<Character> createCharacterStack(int capacity) {
-
-      return stack;
-   }
+        return new StackImplementation<>(capacity);
+    }
 }
